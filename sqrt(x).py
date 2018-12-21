68347dd9107cf9ed1b2b5aa12f1b1bf0f8b275f3
Mehak <mehaktawakley@gmail.com>
2018-12-21 17:53:13 +0530

    init commit

--- sqrt(x).py
+++ sqrt(x).py
@@ -1,20 +1,23 @@
 """
-Problem Link: https://leetcode.com/problems/sqrtx/
-
 Implement int sqrt(int x).
+
 Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
 Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
 
 Example 1:
+
 Input: 4
 Output: 2
-
 Example 2:
+
 Input: 8
 Output: 2
+
 Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
 """
-class Solution(object):
+
+class Solution:
     def mySqrt(self, x):
         """
         :type x: int
@@ -22,14 +25,17 @@ class Solution(object):
         """
         if x == 0:
             return 0
-        left, right = 1, x
-        while left <= right:
-            mid = (left + right) // 2
-            squared = mid * mid
-            if squared == x:
+        
+        start, end = 1, x
+        while (start <= end):
+            mid = (start+end) // 2
+            sqrt = mid * mid
+            if sqrt == x:
                 return mid
-            elif squared > x:
-                right = mid - 1
             else:
-                left = mid + 1
-        return right
+                if sqrt > x:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+        return end
+
