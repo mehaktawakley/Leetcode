8064243e7ddc5832ec77b1134dcb11b16d3bbda9
Mehak Tawakley <32432186+mehaktawakley@users.noreply.github.com>
2019-01-10 23:36:30 +0530

    init commit

--- NimGame.py
+++ NimGame.py
@@ -10,7 +10,6 @@ Explanation: If there are 4 stones in the heap, then you will never win the game
              No matter 1, 2, or 3 stones you remove, the last stone will always be 
              removed by your friend.
 """
-
 class Solution:
     def canWinNim(self, n):
         """
