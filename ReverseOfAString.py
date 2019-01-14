6a76d0f9ace4831bba4a9c11cde7ea0e9053101f
Mehak Tawakley <32432186+mehaktawakley@users.noreply.github.com>
2019-01-14 23:16:26 +0530

    Update ReverseOfAString.py

--- ReverseOfAString.py
+++ ReverseOfAString.py
@@ -4,8 +4,6 @@ Write a function that takes a string as input and returns the string reversed.
 Example:
 Given s = "hello", return "olleh".
 """
-
 class Solution:
     def reverseString(self, s):
         return s[::-1]
-        
